9b8cf2551f11326e6b67b6a8f39060f77524e56d
sandunrmst <sandunalugc@gmail.com>
2024-01-31 13:39:52 +0530

    use global context

--- src/components/componentone.jsx
+++ src/components/componentone.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context";
 
 const Componentone = () => {
+  const { setTheme, theme } = useContext(GlobalContext);
+
+  console.log(theme);
   return (
     <div>
-      {" "}
       <h1 className="text-3xl text-orange-400 font-bold mb-5">Use Context</h1>
-      <button className="p-2 bg-orange-400 rounded-md mt-2 font-semibold text-yellow-50 hover:bg-orange-500 duration-200">
+      <button
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        className="p-2 bg-orange-400 rounded-md mt-2 font-semibold text-yellow-50 hover:bg-orange-500 duration-200"
+      >
         Change Theme
       </button>
     </div>
--- src/components/componenttwo.jsx
+++ src/components/componenttwo.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context";
 
 const Componenttwo = () => {
+  const { theme } = useContext(GlobalContext);
+
   return (
     <div>
       <h1 className="text-3xl text-orange-400 font-bold mb-5">
@@ -10,7 +13,14 @@ const Componenttwo = () => {
         <button className="p-2 bg-orange-400 rounded-md mt-2 font-semibold text-yellow-50 hover:bg-orange-500 duration-200">
           Login
         </button>
-        <button className="p-2 bg-orange-400 rounded-md mt-2 font-semibold text-yellow-50 hover:bg-orange-500 duration-200">
+        <button
+          style={
+            theme === "dark"
+              ? { backgroundColor: "black", color: "#fff" }
+              : { backgroundColor: "orange", color: "#000" }
+          }
+          className="p-2 bg-orange-400 rounded-md mt-2 font-semibold text-yellow-50 hover:bg-orange-500 duration-200"
+        >
           Register
         </button>
       </div>
