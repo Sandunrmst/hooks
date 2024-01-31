b36443685c0bcd146006663260db8e4ca109b9b0
sandunrmst <sandunalugc@gmail.com>
2024-01-31 14:16:31 +0530

    build funtion with use reducer hook

--- src/components/hookusereducer.jsx
+++ src/components/hookusereducer.jsx
@@ -2,9 +2,37 @@ import React, { useReducer } from "react";
 
 const initialState = {
   count: 0,
+  showCountFlag: true,
 };
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  console.log(state, action);
+
+  switch (action.type) {
+    case "add":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "substract":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case "reset":
+      return {
+        ...state,
+        count: 0,
+      };
+    case "showcount":
+      return {
+        ...state,
+        showCountFlag: !state.showCountFlag,
+      };
+    default:
+      break;
+  }
+}
 
 const Hookusereducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -15,17 +43,30 @@ const Hookusereducer = () => {
       <h1 className="text-3xl text-orange-400 font-bold mb-5">
         Use Reducer Hook
       </h1>
+      {state.showCountFlag ? <h2>Count is {state.count}</h2> : null}
       <div className="flex gap-5 justify-center">
-        <button className="p-3 bg-yellow-400 rounded-md  hover:bg-yellow-500">
+        <button
+          onClick={() => dispatch({ type: "add" })}
+          className="p-3 bg-yellow-400 rounded-md  hover:bg-yellow-500"
+        >
           Increacse Count
         </button>
-        <button className="p-3 bg-yellow-400 rounded-md hover:bg-yellow-500">
+        <button
+          onClick={() => dispatch({ type: "substract" })}
+          className="p-3 bg-yellow-400 rounded-md hover:bg-yellow-500"
+        >
           Decrease Count
         </button>
-        <button className="p-3 bg-yellow-400 rounded-md  hover:bg-yellow-500">
+        <button
+          onClick={() => dispatch({ type: "reset" })}
+          className="p-3 bg-yellow-400 rounded-md  hover:bg-yellow-500"
+        >
           Reset Count
         </button>
-        <button className="p-3 bg-yellow-400 rounded-md  hover:bg-yellow-500">
+        <button
+          onClick={() => dispatch({ type: "showcount" })}
+          className="p-3 bg-yellow-400 rounded-md  hover:bg-yellow-500"
+        >
           Show Count
         </button>
       </div>
